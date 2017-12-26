3ea3f4ba759a0d8d3b28c048ddbecf9a6d4f66c5
Akash <akash.darknight@gmail.com>
2017-12-26 10:03:00 +0530

    Main Script

--- Main file.py
+++ Main file.py
@@ -39,3 +39,39 @@ index = 27
 plt.imshow(train_set_x_orig[index])
 print("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode(
     "utf-8") + "' picture.")
+
+
+### START CODE HERE ### (≈ 3 lines of code)
+m_train = train_set_x_orig.shape[0]
+m_test = test_set_x_orig.shape[0]
+num_px = train_set_x_orig.shape[1]
+### END CODE HERE ###
+
+print("Number of training examples: m_train = " + str(m_train))
+print("Number of testing examples: m_test = " + str(m_test))
+print("Height/Width of each image: num_px = " + str(num_px))
+print("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+print("train_set_x shape: " + str(train_set_x_orig.shape))
+print("train_set_y shape: " + str(train_set_y.shape))
+print("test_set_x shape: " + str(test_set_x_orig.shape))
+print("test_set_y shape: " + str(test_set_y.shape))
+
+# **Expected Output for m_train, m_test and num_px**:
+# <table style="width:15%">
+#   <tr>
+#     <td>**m_train**</td>
+#     <td> 209 </td>
+#   </tr>
+#
+#   <tr>
+#     <td>**m_test**</td>
+#     <td> 50 </td>
+#   </tr>
+#
+#   <tr>
+#     <td>**num_px**</td>
+#     <td> 64 </td>
+#   </tr>
+#
+# </table>
+#
